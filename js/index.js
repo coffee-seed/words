6020a5e0ccb6991d8ab0cd0f22257f571a2c5273
Grigoriy Efimov <efimov-gm@newpage.xyz>
2020-12-02 16:55:18 +0300

    add frame on click

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 words_area.innerHTML="";
 words.forEach(word => words_area.innerHTML=words_area.innerHTML+"<img src="+word['image']+
-" onclick='var audio = new Audio(\" "+word['sound'] +" \"); audio.play();'>"
+" onclick='this.style.border=\"2vh #0FBB0F solid\"; that=this; setTimeout(function(){that.style.border=\"none\";},350); var audio = new Audio(\" "+word['sound'] +" \"); audio.play();'>"
 );
